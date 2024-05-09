e2df05fafe837bd26edf4387aa0fc13dbaf22562
RaptorCentauri <dave.leo.shilander@icloud.com>
2024-05-09 14:43:15 -0500

    Save Work

--- src/main/java/com/bluedemons2024/dolphintellect_backend/Account/Account.java
+++ src/main/java/com/bluedemons2024/dolphintellect_backend/Account/Account.java
@@ -1,70 +1,59 @@
-//package com.daveleoshilander.springpractice.Account;
-//
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//@Table(name = "accounts")
-//public class Account {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    int id;
-//
-//    String email;
-//    String password;
-//
-//    @Column(name = "student_id")
-//    long studentID;
-//
-//
-//    public Account(String email, String password, long studentID) {
-//        this.email = email;
-//        this.password = password;
-//        this.studentID = studentID;
-//    }
-//
-//    public Account() {
-//
-//    }
-//
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public long getStudentID() {
-//        return studentID;
-//    }
-//
-//    public void setStudentID(long studentID) {
-//        this.studentID = studentID;
-//    }
-//
-//
-//
-//
-//
-//
-//
-//}
+package com.bluedemons2024.dolphintellect_backend.Account;
+import jakarta.persistence.*;
+
+@Entity(name="mysql")
+@Table(name="accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String email;
+    String password;
+
+    @Column(name="student_id")
+    String studentID;
+
+    public Account(int id, String email, String password, String studentID) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.studentID = studentID;
+    }
+
+    public Account() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+}
--- src/main/java/com/bluedemons2024/dolphintellect_backend/Account/AccountRepository.java
+++ src/main/java/com/bluedemons2024/dolphintellect_backend/Account/AccountRepository.java
@@ -1,6 +1,6 @@
-//package com.daveleoshilander.springpractice.Account;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AccountRepository extends JpaRepository<Account, Integer> {
-//}
+package com.bluedemons2024.dolphintellect_backend.Account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+}
